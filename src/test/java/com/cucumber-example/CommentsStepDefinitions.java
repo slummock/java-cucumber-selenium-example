0ad9a7b2ef9b21d53cc3555eda62c070270ec604
slummock <slummock@gmail.com>
2018-01-18 10:02:01 +0000

    selenium in a working state with geckodriver for firefox ready to write browser automation

--- src/test/java/com/cucumber-example/CommentsStepDefinitions.java
+++ src/test/java/com/cucumber-example/CommentsStepDefinitions.java
@@ -1,11 +1,23 @@
+import java.util.concurrent.TimeUnit;
+
 import cucumber.api.java8.En;
 import cucumber.api.PendingException;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class CommentsStepDefinitions implements En{
     public CommentsStepDefinitions(){
         Given("^User has logged in$", () -> {
             // Write code here that turns the phrase above into concrete actions
-            throw new PendingException();
+            String currentDirectory = System.getProperty("user.dir");
+            String geckodriverLocation = currentDirectory +"\\tools\\geckodriver.exe";
+            System.setProperty("webdriver.gecko.driver", geckodriverLocation);
+            WebDriver driver = new FirefoxDriver();
+            driver.get("http://www.google.com");
+            System.out.println("test");
         });
         
         Given("^Navigated to the post page$", () -> {
@@ -17,7 +29,7 @@ public class CommentsStepDefinitions implements En{
             // Write code here that turns the phrase above into concrete actions
             throw new PendingException();
         });
-        
+
         Then("^Comment appears on the page$", () -> {
             // Write code here that turns the phrase above into concrete actions
             throw new PendingException();
